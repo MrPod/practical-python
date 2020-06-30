02a51c5f5b3d2ecf6b8a90e1436627dac9d637d2
Sam <pod.haulm@gmail.com>
2020-06-30 20:55:11 +0300

    first chapter is done!

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+import sys
+
+def portfolio_cost(filename):
+    with open (filename, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        s = 0
+
+        for line in csv_reader:
+            try:
+                s += int(line['shares']) * float(line['price'])
+            except ValueError:
+                pass
+        return s
+
+if len(sys.argv) == 2:
+    print(sys.argv)
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print(cost)
